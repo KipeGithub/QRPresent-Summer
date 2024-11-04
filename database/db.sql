74d617fe19f5d1f01d7eb982cb339866cd52b57e
KipeGithub <rizkypratamah01@gmail.com>
2024-11-04 23:43:06 +0700

    Dari Laptop sudah ada login sistem dan main dataset 3

--- database/db.sql
+++ database/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2024 at 10:34 AM
+-- Generation Time: Nov 04, 2024 at 05:42 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` enum('0','1') NOT NULL COMMENT '0 = Superadmin, 1 = Admin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`id`, `email`, `password`, `level`) VALUES
+(1, 'superadmin@example.com', '$2y$10$Ephxvp8GnW8q5MBm2j3zMuJ3wz/B/Op5ziw6SHd6HoIdSYyFk5qxS', '0'),
+(2, 'admin@example.com', '$2y$12$QjSH496pcT5CEbzjD/vtVeH03tfHKFy36d4J0Ltp3lRtee9HDxY3K', '1');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `peserta_master`
 --
 
@@ -36,6 +57,7 @@ CREATE TABLE `peserta_master` (
   `contact` varchar(15) DEFAULT NULL,
   `plotting` varchar(50) DEFAULT NULL,
   `status_peserta` varchar(20) DEFAULT NULL,
+  `barcode` varchar(225) DEFAULT NULL,
   `status_presensi` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -43,12 +65,19 @@ CREATE TABLE `peserta_master` (
 -- Dumping data for table `peserta_master`
 --
 
-INSERT INTO `peserta_master` (`id_peserta`, `nama_lengkap`, `nama_depan`, `nama_belakang`, `kelas`, `contact`, `plotting`, `status_peserta`, `status_presensi`) VALUES
-(1, 'Amelia Putri Noviansyah', 'Amelia', 'Noviansyah', 'X ULW', '6283840416978', 'BUS-1', 'SISWA', 'PREPARE');
+INSERT INTO `peserta_master` (`id_peserta`, `nama_lengkap`, `nama_depan`, `nama_belakang`, `kelas`, `contact`, `plotting`, `status_peserta`, `barcode`, `status_presensi`) VALUES
+(1, 'Amelia Putri Noviansyah', 'Amelia', 'Noviansyah', 'X ULW', '6283840416978', 'BUS-1', 'SISWA', NULL, 'PREPARE');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `peserta_master`
@@ -59,6 +88,12 @@ ALTER TABLE `peserta_master`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `peserta_master`
